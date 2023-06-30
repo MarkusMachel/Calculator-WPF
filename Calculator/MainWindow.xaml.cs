4308ee7d6962ff701aee79b5762876d21181bc98
Markus Machel <machelmarkus@gmail.com>
2023-06-30 19:26:14 -0300

    number and operations handler

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,22 +20,73 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            resultLabel.Content = "123456";
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalButton.Click += EqualButton_Click;
         }
 
-        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
+            throw new NotImplementedException();
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void OperationButton_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender == zeroButton) selectedValue = 0;
+            if (sender == oneButton) selectedValue = 1;
+            if (sender == twoButton) selectedValue = 2;
+            if (sender == threeButton)  selectedValue = 3;
+            if (sender == fourButton) selectedValue = 4;
+            if (sender == fiveButton) selectedValue = 5;
+            if (sender == sixButton) selectedValue = 6;
+            if (sender == sevenButton) selectedValue = 7;
+            if (sender == eigthButton) selectedValue = 8;
+            if (sender == nineButton) selectedValue = 9;
+
             if (resultLabel.Content.ToString() == "0")
             {
-                resultLabel.Content = "7";
+                resultLabel.Content = $"{selectedValue}";
             }
             else
             {
-                resultLabel.Content = $"{resultLabel.Content}7 ";
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
             }
         }
     }
